54f2575e45348dcd1b74536c0b7826f1369f508a
Shekh Rehan <shekhrehanayaz@gmail.com>
2025-06-15 15:08:57 +0530

    Update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
       id="about"
       className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
     >
-         <div className="flex flex-col-reverse md:flex-row lg:gap-4 justify-between items-center">
+      <div className="flex flex-col-reverse md:flex-row lg:gap-4 justify-between items-center">
         {/* Left Side */}
         <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
           {/* Greeting */}
@@ -23,27 +23,29 @@ const About = () => {
           {/* Skills Heading with Typing Effect */}
           <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
             <span className="text-white">I am a </span>
-           <TypingEffect
-  texts={[
-    'Full Stack Developer',
-    'UI/UX Designer',
-    'Coder',
-  ]}
-  speed={100}
-  eraseSpeed={50}
-  delay={2000}
-/>
+            <TypingEffect
+              texts={[
+                'Full Stack Developer',
+                'UI/UX Designer',
+                'Coder',
+              ]}
+              speed={100}
+              eraseSpeed={50}
+              delay={2000}
+            />
 
           </h3>
           {/* About Me Paragraph */}
           <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
-          I'm a passionate Full-Stack Developer (Fresher) with strong knowledge of building scalable and user-friendly web applications. Skilled in the MERN stack (MongoDB, Express.js, React.js, Node.js) and other modern technologies, I focus on creating seamless user experiences and clean, efficient code. Currently gaining practical experience through an internship, I’m eager to contribute to real-world projects and grow as a professional developer.
+            I’m a Full-Stack Developer with hands-on experience in building scalable and user-friendly web applications using the MERN stack (MongoDB, Express.js, React.js, Node.js) and other modern technologies like Redux, Tailwind CSS, and Socket.io.
+            During my internship at Hanumant Technology Pvt. Ltd., I worked on real-world projects where I wrote clean, efficient code and helped deliver seamless user experiences.
+            I;m passionate about problem-solving, learning new technologies, and contributing to fast-paced development teams.
 
 
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1_pLl2wjYVCU-wnqXIhjhYr0YC0SJXvwv/view?usp=sharing"
+            href="https://drive.google.com/file/d/1lbOj4KBKXP-7U9YP3FAQMaScfwl4GTuL/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
@@ -54,7 +56,7 @@ const About = () => {
           >
             DOWNLOAD CV
           </a>
-          
+
         </div>
         {/* Right Side */}
         <div className="md:w-1/2 flex justify-center md:justify-end">
@@ -75,7 +77,7 @@ const About = () => {
           </Tilt>
         </div>
       </div>
-   
+
     </section>
   );
 };
